6a5ec6d15f5994360a7bd2184aeaa4f36d255991
Alex Drlica-Wagner <kadrlica@fnal.gov>
2015-12-03 16:00:16 -0600

    Update version number in preparation for tag

--- python/authlist.py
+++ python/authlist.py
@@ -17,7 +17,7 @@ affiliations. These need to be corrected by hand in the CSV file.
 """
 __author__ = "Alex Drlica-Wagner"
 __email__ = "kadrlica@fnal.gov"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 import csv
 import numpy as np
